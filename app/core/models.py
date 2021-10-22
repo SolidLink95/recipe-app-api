ed6c554a0b887d441209c2de2a0138988835d316
banan039pl <mateusz.banaszewski18@gmail.com>
2021-10-22 09:24:28 +0200

    add files

---
+++ app/core/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+    PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+    def create_user(self,email,password=None):
