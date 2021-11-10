710a28844d967a39444ecf33465057cf958fec6b
Mati <banan039eu@gmail.com>
2021-11-10 10:06:34 +0100

    Commit

--- app/core/models.py
+++ app/core/models.py
@@ -5,9 +5,10 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 
 
 class UserManager(BaseUserManager):
-    def create_user(self,email,password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
-        if not email: raise ValueError('Users must have an email address')
+        if not email:
+            raise ValueError('Users must have an email address')
 
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
@@ -23,6 +24,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
