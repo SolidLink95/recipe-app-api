e312e270388f3a2771611f297045245bb9be81a6
mati <mateusz.banaszewski18@gmail.com>
2022-02-14 10:45:41 +0100

    Added working list tags endpoint

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -75,7 +75,7 @@ class PublicUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_token_no_user(self):
-        """Test that token is not created if user doesnt exist"""
+        """Test that token is not created if user doesn't exist"""
         payload = {'email': 'test@gmail.com', 'password': 'testpass'}
         res = self.client.post(TOKEN_URL, payload)
 
@@ -123,7 +123,7 @@ class PrivateUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_update_user_profile(self):
-        """Test updating the user profile for authnticated user"""
+        """Test updating the user profile for authenticated user"""
         payload = {'name': 'new name', 'password': 'newpassword123'}
 
         res = self.client.patch(ME_URL, payload)
